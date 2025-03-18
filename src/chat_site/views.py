fdeb43a8ba49dfe9bfaad578226797631f2e0ca7
Gopal Khadka <khadkagopal.mail@gmail.com>
2025-03-18 08:29:09 +0545

    Add feature to send messasge in the group

--- src/chat_site/views.py
+++ src/chat_site/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpRequest
 from .models import ChatGroup
+from .forms import ChatMessageCreateForm
 
 
 @login_required
-def chat_view(request):
+def chat_view(request: HttpRequest):
     chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
     chat_messages = chat_group.chat_messages.order_by("created")[:30]
-    return render(request, "chat_site/chat.html", {"chat_messages": chat_messages})
+
+    form = ChatMessageCreateForm()
+    if request.method == "POST":
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid():
+            # before saving msg, we need to add author and chatgroup value to it
+            # So commit = False
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            return redirect("chat_home")
+
+    return render(
+        request, "chat_site/chat.html", {"chat_messages": chat_messages, "form": form}
+    )
